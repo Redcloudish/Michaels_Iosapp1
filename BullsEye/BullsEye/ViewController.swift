351393729f3b59770cbee5fb539533db842bc287
Michael Brown <160866598+Redcloudish@users.noreply.github.com>
2024-05-23 00:07:46 -0400

    newer interface

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -8,27 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var slider: UISlider!
+    
+    @IBOutlet var targetLabel: UILabel!
 
+    var currentValue: Int = 0
+    var targetValue = 0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+      super.viewDidLoad()
+      startNewRound()  // Replace previous code with this
     }
 
-  @IBAction func showAlert() {
-      let alert = UIAlertController(
-          title: "Hello, World",
-          message: "This is my first app!",
-          preferredStyle: .alert)
-
-        let action = UIAlertAction(
-          title: "Awesome",
-          style: .default,
-          handler: nil)
-
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-      } 
     
+    
+    @IBAction func showAlert() {
+        var difference: Int
+          
+          if currentValue > targetValue {
+            difference = currentValue - targetValue
+          } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+          } else {
+            difference = 0
+          }
+
+          let message = "The value of the slider is: \(currentValue)" +
+                        "\nThe target value is: \(targetValue)" +
+                        "\nThe difference is: \(difference)" 
+        
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: message,    // changed
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "OK",          // changed
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+}
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()  // Add this line
+    }
+
+    
+    func updateLabels() {
+      targetLabel.text = String(targetValue)
+    }
 }
 
